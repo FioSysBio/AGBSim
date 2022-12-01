a53df2a703ba25670f6e670e0e981e2edd5aa2b4
Thiago Giannini Ramos <gianninimail@gmail.com>
2022-12-01 06:40:32 -0300

    Implementação Classe SimulationResult

--- src/main/java/br/fiocruz/procc/acbmservice/domain/SimulationResult.java
+++ src/main/java/br/fiocruz/procc/acbmservice/domain/SimulationResult.java
@@ -1,4 +1,25 @@
 package br.fiocruz.procc.acbmservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class SimulationResult {
+
+    @Id
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "simulation_id")
+    private Simulation simulation;
 }
