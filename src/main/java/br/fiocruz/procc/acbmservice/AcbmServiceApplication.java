834f0a3d7b1ccd9b97c0cb13748fc53a99bfdbbb
Thiago Giannini Ramos <gianninimail@gmail.com>
2022-12-14 16:59:30 -0300

    Implementação da Framework OPEN API para geração semi-automática de documentação.

--- src/main/java/br/fiocruz/procc/acbmservice/AcbmServiceApplication.java
+++ src/main/java/br/fiocruz/procc/acbmservice/AcbmServiceApplication.java
@@ -1,8 +1,11 @@
 package br.fiocruz.procc.acbmservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "AGBSim API", version = "1.0", description = "API for agent-based simulation system."))
 @SpringBootApplication
 public class AcbmServiceApplication {
 
--- src/main/java/br/fiocruz/procc/acbmservice/controller/MainController.java
+++ src/main/java/br/fiocruz/procc/acbmservice/controller/MainController.java
@@ -1,17 +1,42 @@
 package br.fiocruz.procc.acbmservice.controller;
 
 import br.fiocruz.procc.acbmservice.domain.SimulationResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Main Controll", description = "ROUTES for API operations.")
 @RestController
 @RequestMapping("/")
 public class MainController {
 
-    @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "JSON Array of Simulation Results registered in the database."),
+            @ApiResponse(responseCode = "403", description = "No permission to access this resource."),
+            @ApiResponse(responseCode = "404", description = "Resource not found in the database."),
+            @ApiResponse(responseCode = "500", description = "An internal exception was generated on the Server."),
+    })
+    @Operation(description = "Searches all Simulation Results registered in the database.")
+    @GetMapping("/results")
     public ResponseEntity<SimulationResult> get() {
+
+        return ResponseEntity.ok(new SimulationResult());
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "JSON of Simulation Result registered in the database."),
+            @ApiResponse(responseCode = "403", description = "No permission to access this resource."),
+            @ApiResponse(responseCode = "404", description = "Resource not found in the database."),
+            @ApiResponse(responseCode = "500", description = "An internal exception was generated on the Server."),
+    })
+    @Operation(description = "Search by ID Simulation Result registered in the database.")
+    @GetMapping("/results/id")
+    public ResponseEntity<SimulationResult> getById() {
 
         return ResponseEntity.ok(new SimulationResult());
     }
