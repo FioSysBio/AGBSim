58d00d07bbf206ce7afbbb541c401d40eca4332e
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-02-14 21:47:20 -0300

    Add Data para Cells e Metabolites

---
+++ src/main/resources/data/metabolites.sql
@@ -0,0 +1,7 @@
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (1, 10, 180, 'Glucose', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (2, 10, 60, 'Acetate', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (3, 10, 88, 'Butyrate', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (4, 10, 46, 'Formate', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (5, 10, 46, 'Ethanol', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (6, 10, 92, 'Glycerol', 8000, 1000);
+INSERT INTO `acbm-service`.metabolite (id, amount, molar_mass, name, speed, uptake_upper_bound) VALUES (7, 10, 300, 'Custom', 8000, 1000);
