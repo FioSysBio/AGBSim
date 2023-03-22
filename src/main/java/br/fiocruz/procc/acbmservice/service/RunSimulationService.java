b8ff2e495a60e987a17f80792d360d85a74e3e53
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-03-21 23:09:34 -0300

    Adicioando campos para metabolitos no ambiente de simulação

--- src/main/java/br/fiocruz/procc/acbmservice/service/RunSimulationService.java
+++ src/main/java/br/fiocruz/procc/acbmservice/service/RunSimulationService.java
@@ -1,16 +1,20 @@
 package br.fiocruz.procc.acbmservice.service;
 
 import br.fiocruz.procc.acbmservice.commands.EnvironmentCommand;
+import br.fiocruz.procc.acbmservice.commands.SimulationItensInfoCommand;
 import br.fiocruz.procc.acbmservice.commands.SimulationRunCommand;
+import br.fiocruz.procc.acbmservice.domain.Bacteria;
 import br.fiocruz.procc.acbmservice.domain.Environment;
 import br.fiocruz.procc.acbmservice.domain.RunWindow;
 import br.fiocruz.procc.acbmservice.domain.enuns.AmountType;
 import br.fiocruz.procc.acbmservice.domain.enuns.ShapeType;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 @Service
@@ -31,6 +35,21 @@ public class RunSimulationService {
 //                }
 //                ex_rxns_direction.add(al2);
 //            }
+
+            if (simulationRunCommand.getItensSimulation() == null || simulationRunCommand.getItensSimulation().size() == 0) {
+                return "Items for simulation are missing!!!";
+            }
+
+            Boolean temItens = true;
+            for (SimulationItensInfoCommand item : simulationRunCommand.getItensSimulation()) {
+                if (item.getCell() == null) {
+                            return "Missing cells for this simulation!!!";
+                }
+
+                if (item.getMetabolites() == null || item.getMetabolites().size() == 0) {
+                    return "Missing metabolites for this simulation!!!";
+                }
+            }
 
             Environment.setParameters(transform(simulationRunCommand));
             RunWindow w = new RunWindow();
@@ -100,7 +119,7 @@ public class RunSimulationService {
                             );
                         }
                         //linha 341
-                        if (item.getCell().getMass() != 0.0 || item.getCell().getMass() != null) {
+                        if (item.getCell().getMass() == 0.0 || item.getCell().getMass() == null) {
                             command.getM_bac().add(
                                     (1.1) *
                                             command.getV_bac().get( command.getV_bac().size() - 1) *
@@ -130,16 +149,57 @@ public class RunSimulationService {
                                 Integer.parseInt(item.getCell().getCellColor().split(",")[2]) //B
                         ));
 
-                        //linha 1225
+                        //Metabolitos
+
                         item.getMetabolites().forEach(met -> {
+                            //linha 785
+                            {
+                                command.getMetabolite_name().add(met.getName());
+//                                metabolite_name.add((String) metNamecomboBox.getSelectedItem());
+                                if (met.getAmountType() == AmountType.GL) {
+                                    command.getMetabolite_conc().add(Double.parseDouble(met.getAmount()));
+                                    command.getMetabolite_count().add(-1);
+                                } else {
+                                    command.getMetabolite_count().add(Integer.parseInt(met.getAmount()));
+                                    command.getMetabolite_conc().add(-1.0);
+                                }
 
-                            ArrayList<String> al = new ArrayList<String>();
-                            al.add(met.getReactionName());
-                            command.getEx_rxns_name().add(al);
+                                command.getMetabolite_mw().add(met.getMolarMass());
 
-                            ArrayList<Integer> al2 = new ArrayList<Integer>();
-                            al2.add(met.getReactionDirection());
-                            command.getEx_rxns_direction().add(al2);
+                                command.getMetabolite_color().add(
+                                        new Color(
+                                                Integer.parseInt(item.getCell().getCellColor().split(",")[0]),//R
+                                                Integer.parseInt(item.getCell().getCellColor().split(",")[1]),//G
+                                                Integer.parseInt(item.getCell().getCellColor().split(",")[2]) //B
+                                        )
+                                );
+
+
+                                command.getMetabolite_index().add(0);
+
+                                command.getMetabolite_speed().add(met.getSpeed());
+
+                                command.getMetabolite_uub().add(met.getUptakeUpperBound());
+
+//                                dlm2.addElement((String) metNamecomboBox.getSelectedItem());
+//
+//                                int r = new Random().nextInt(255);
+//                                int g = new Random().nextInt(255);
+//                                int b = new Random().nextInt(255);
+//                                Color randomColor = new Color(r, g, b);
+//                                btnMetChooseColor.setBackground(randomColor);
+                            }
+
+                            //linha 1225
+                            {
+                                ArrayList<String> al = new ArrayList<String>();
+                                al.add(met.getReactionName());
+                                command.getEx_rxns_name().add(al);
+
+                                ArrayList<Integer> al2 = new ArrayList<Integer>();
+                                al2.add(met.getReactionDirection());
+                                command.getEx_rxns_direction().add(al2);
+                            }
                         });
 
                         //linha 1125
@@ -154,6 +214,26 @@ public class RunSimulationService {
                         command.setW(simulationRunCommand.getEnvironmentWidth());
 
                         command.setN_real(simulationRunCommand.getMetaboliteScale() * Math.pow(10, simulationRunCommand.getMetaboliteScaleMult()));
+
+                        //nToN
+                        {
+                            double V = command.getL() * command.getW() * command.getD() * Math.pow(10, -18);
+                            for (int i = 0; i < command.getBacteria_conc().size(); i++) {
+                                if (command.getBacteria_conc().get(i) != -1 ) {
+//                                    System.out.println(bacteria_conc.get(i)+ ","+m_bac.get(i)+","+bacteria_scale.get(i));
+                                    int n = (int) ( (command.getBacteria_conc().get(i)* 1000 * V) /( command.getM_bac().get(i) ) );
+                                    command.getBacteria_count().set(i, n);
+                                }
+                            }
+
+                            for (int i = 0; i < command.getMetabolite_conc().size(); i++) {
+                                if (command.getMetabolite_conc().get(i) != -1 ) {
+                                    int n = (int) ( ((command.getMetabolite_conc().get(i)*1000 * V) / (command.getMetabolite_mw().get(i))) * ( Bacteria.n_a / command.getN_real())  );
+                                    command.getMetabolite_count().set(i, n);
+                                }
+                            }
+                        }
+
                     }
             );
         } catch (Exception ex) {
