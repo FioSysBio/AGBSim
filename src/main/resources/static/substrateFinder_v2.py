50312496bc8d94c78606653a593a18924c02f611
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-04-04 20:39:02 -0300

    Ajustes no script substrateFinder.

--- src/main/resources/static/substrateFinder_v2.py
+++ src/main/resources/static/substrateFinder_v2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from cobra.io import load_model, load_json_model, save_json_model, load_matlab_model, save_matlab_model, read_sbml_model, write_sbml_model
+from cobra.io import load_model, load_json_model, read_sbml_model
 
 def substrateFinder(reactions, directions, file_metabolic_model):
 
