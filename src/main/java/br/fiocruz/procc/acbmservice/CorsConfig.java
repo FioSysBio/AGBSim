1ec91121cb99d884beaefa153c96e95a7f6c2528
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-06-23 09:29:16 -0300

    Alterando configuração de CORs

--- src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
+++ src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
