db221e8a6917e2ba9638775bad5c08837e8598a0
Thiago Giannini Ramos <gianninimail@gmail.com>
2023-06-23 10:47:48 -0300

    Colando certificado na API

--- src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
+++ src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
@@ -10,9 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+        registry.addMapping("/subpasta/**") // Substitua "/subpasta/**" pelo caminho da sua subpasta
+                .allowedOrigins("*") // Defina os domínios permitidos ou use "*" para permitir todos
+                .allowedMethods("*") // Defina os métodos HTTP permitidos
+                .allowedHeaders("*") // Defina os cabeçalhos permitidos ou use "*" para permitir todos
+                .allowCredentials(true) // Permitir envio de cookies de origens diferentes
+                .maxAge(3600); // Tempo de validade do preflight request em segundos
     }
 }
