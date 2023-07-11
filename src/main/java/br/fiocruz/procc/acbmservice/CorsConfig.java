10fdbe86229b00b080fdc0a04a1978dd943d9d61
Thiago Ramos <thiago.ramos@impa.br>
2023-07-11 07:14:47 -0300

    Limpeza de código

--- src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
+++ src/main/java/br/fiocruz/procc/acbmservice/CorsConfig.java
@@ -10,13 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-//        registry.addMapping("/agbsim/**") // Substitua "/subpasta/**" pelo caminho da sua subpasta
-//                .allowedOrigins("*") // Defina os domínios permitidos ou use "*" para permitir todos
-//                .allowedMethods("*") // Defina os métodos HTTP permitidos
-//                .allowedHeaders("*") // Defina os cabeçalhos permitidos ou use "*" para permitir todos
-//                .allowCredentials(true) // Permitir envio de cookies de origens diferentes
-//                .maxAge(3600); // Tempo de validade do preflight request em segundos
-
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
